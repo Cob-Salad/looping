91d0a952548acea62b7bf290ea199b7422a12d82
Colby <156106733+Cob-Salad@users.noreply.github.com>
2024-04-18 09:05:23 -0600

    Add files via upload

--- reverse_string.py
+++ reverse_string.py
@@ -7,10 +7,27 @@ def reverse_while(s):
             n += s[i]
     return n
 
+def reverse_for_each(s):
+    n = ""
+    for char in s:
+        n = char + n
+    return n
+
+def reverse_for(s):
+    string = ""
+    for i in range(len(s)):
+        string = s[i] + string
+
+    return string
+
 
 
 def main():
     print(f"{reverse_while('hello')}")
+    print(f"{reverse_for('hey there')}")
+    print(f"{reverse_for_each('hello world')}")
+
+
 
 if __name__ == "__main__":
     main()
